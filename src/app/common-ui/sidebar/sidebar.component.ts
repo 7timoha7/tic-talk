c9fe5ce7cbbe487c882ab430b02d0f398735d3b7
Artem Markelov <artem77timoha77@gmail.com>
2024-09-05 01:11:18 +0600

    angular - Сделал страницу логина и вывод карточек юзеров .

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,20 +1,54 @@
 import {Component, inject} from '@angular/core';
 import {ProfileService} from "../../data/services/profile.service";
+import {AsyncPipe, JsonPipe, NgForOf, NgOptimizedImage} from "@angular/common";
+import {SvgIconComponent} from "../svg-icon/svg-icon.component";
+import {SubscriberCardComponent} from "../subsriber-card/subscriber-card.component";
+import {RouterLink} from "@angular/router";
+import {firstValueFrom} from "rxjs";
+import {ImgUrlPipe} from "../../helpers/pipes/img-url.pipe";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    SvgIconComponent,
+    NgForOf,
+    SubscriberCardComponent,
+    AsyncPipe,
+    JsonPipe,
+    RouterLink,
+    ImgUrlPipe
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
   profileService = inject(ProfileService);
 
+  subscribers$ = this.profileService.getSubscribersShortList()
+  me = this.profileService.me
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: 'chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    },
+  ]
+
+
   ngOnInit() {
-    console.log('ngOnInit');
-    this.profileService.getMe().subscribe(val => {
-      console.log(val)
-    })
+    firstValueFrom(this.profileService.getMe())
   }
 }
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -1,14 +1,16 @@
-import {inject, Injectable} from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Profile} from "../interfaces/profile.interface";
+import {Pageble} from "../interfaces/pageble.interface";
+import {map, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   http = inject(HttpClient);
-
   baseApiUrl = 'https://icherniakov.ru/yt-course/';
+  me = signal<Profile | null>(null)
 
   constructor() {
   }
@@ -17,7 +19,17 @@ export class ProfileService {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
   }
 
+  getSubscribersShortList() {
+    return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}account/subscribers/`)
+      .pipe(
+        map(res => res.items.slice(0, 3))
+      )
+  }
+
   getMe() {
     return this.http.get<Profile>(`${this.baseApiUrl}account/me`)
+      .pipe(
+        tap(res => this.me.set(res))
+      )
   }
 }
